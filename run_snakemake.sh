39423b35c714264e65e82151ed1151f92280b885
LXRz1 <133985740+LXRz1@users.noreply.github.com>
2023-08-18 15:41:30 +0800

    Update run_snakemake.sh

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -1,2 +1,1 @@
-#/home3/2812743l/mambaforge/envs/snakemake/bin/snakemake -s Snakefile --configfile config.yaml --cores 8
-/home3/2812743l/mambaforge/envs/snakemake/bin/snakemake -s Snakefile --cores 8
+/path/to/snakemake -s /path/to/Snakefile --configfile /path/to/config.yaml --cores 8
