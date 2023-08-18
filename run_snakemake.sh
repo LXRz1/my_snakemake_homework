63d87aa2f279c367f5fbb9054a0c9e3ed382949c
LXRz1 <cirilee2022@gmail.com>
2023-08-18 08:30:06 +0100

    update

---
+++ run_snakemake.sh
@@ -0,0 +1,2 @@
+#/home3/2812743l/mambaforge/envs/snakemake/bin/snakemake -s Snakefile --configfile config.yaml --cores 8
+/home3/2812743l/mambaforge/envs/snakemake/bin/snakemake -s Snakefile --cores 8
